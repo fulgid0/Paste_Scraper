56b16a6a7dac9cb32aa71e0fde0c045b2da43e65
fulgid0 <118464641+fulgid0@users.noreply.github.com>
2024-02-12 16:38:12 +0100

    Create DB_creation.py

---
+++ DB_creation.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import sqlite3
+
+# Connect to SQLite database (or create it if it doesn't exist)
+conn = sqlite3.connect('Paste_Scraper.db')
+print("Opened database successfully")
+
+# Create Words table
+conn.execute('''CREATE TABLE Words (
+        WordID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Word TEXT NOT NULL,
+        UsageCount INTEGER DEFAULT 0
+);''')
+
+# Create ScrapedContent table
+conn.execute('''CREATE TABLE ScrapedContent (
+        ContentID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Word1ID INTEGER NOT NULL,
+        Word2ID INTEGER NOT NULL,
+        ScrapedText TEXT NOT NULL,
+        ScrapedDateTime TEXT NOT NULL,
+        URL TEXT NOT NULL,
+        FOREIGN KEY (Word1ID) REFERENCES Words(WordID),
+        FOREIGN KEY (Word2ID) REFERENCES Words(WordID)
+);''')
+
+# Create AnalysisResults table
+conn.execute('''CREATE TABLE AnalysisResults (
+        AnalysisID INTEGER PRIMARY KEY AUTOINCREMENT,
+        ContentID INTEGER NOT NULL,
+        Keyword1 TEXT NOT NULL,
+        Keyword2 TEXT NOT NULL,
+        Keyword3 TEXT NOT NULL,
+        Alert TEXT DEFAULT NULL,
+        AnalysisDateTime TEXT NOT NULL,
+        FOREIGN KEY (ContentID) REFERENCES ScrapedContent(ContentID)
+);''')
+
+print("Tables created successfully")
+
+# Close the connection to the database
+conn.close()
